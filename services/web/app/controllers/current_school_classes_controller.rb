d5e2d4f0cc7c111a9718f99acfe5ff4c7cff0e23
Fredrik Teschke <f@ftes.de>
2020-04-30 13:39:34 +0200

    ✨ Redirect to lesson evaluation when switching class

--- services/web/app/controllers/current_school_classes_controller.rb
+++ services/web/app/controllers/current_school_classes_controller.rb
@@ -3,6 +3,8 @@ class CurrentSchoolClassesController < ApplicationController
     school_class_id = params.dig(:current_school_class, :school_class_id)
     school_class = SchoolClass.find(school_class_id)
     CurrentSchoolClass.set(school_class)
-    redirect_back fallback_location: root_path, notice: t('.notice')
+
+    redirect_path = lesson_evaluate_path(school_class.most_recent_lesson_or_create)
+    redirect_to redirect_path, notice: t('.notice')
   end
 end
